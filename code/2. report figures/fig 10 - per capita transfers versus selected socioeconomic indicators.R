7873d907db5e9debc388b68b5591e89fcf268fbf
Sarah Eckhardt <167136820+SarahMEckhardt@users.noreply.github.com>
2024-09-17 15:18:11 -0400

    Update fig 10 - per capita transfers versus selected socioeconomic indicators.R

--- code/2. report figures/fig 10 - per capita transfers versus selected socioeconomic indicators.R
+++ code/2. report figures/fig 10 - per capita transfers versus selected socioeconomic indicators.R
@@ -39,11 +39,16 @@ transfers = left_join(transfers, poverty, by = c("year", "GeoFIPS"))
 
 scatter_dynamic = transfers %>%
   filter(year == 2022 | year == 1970) %>%
-  select(share_65_over, GeoFIPS,year,Percent.below.poverty.level, transfers_govt_pce_per_capita) %>%
+  select(share_65_over, GeoFIPS,year,Percent.below.poverty.level,
+         transfers_govt_pce_per_capita) %>%
   filter(GeoFIPS !=55078) %>% 
   pivot_wider(id_cols = c("GeoFIPS"),
               names_from = year,
-              values_from = c("share_65_over", "Percent.below.poverty.level", "transfers_govt_pce_per_capita"))%>%
+              values_from = c("share_65_over",
+                              "Percent.below.poverty.level",
+                              "transfers_govt_pce_per_capita"))%>%
+  mutate(Percent.below.poverty.level_2022 = Percent.below.poverty.level_2022/100,
+         Percent.below.poverty.level_1970=Percent.below.poverty.level_1970/100) %>%
   mutate(pov = (Percent.below.poverty.level_2022-Percent.below.poverty.level_1970)/Percent.below.poverty.level_1970,
          old = (share_65_over_2022-share_65_over_1970)/share_65_over_1970,
          transfer = (transfers_govt_pce_per_capita_2022-transfers_govt_pce_per_capita_1970)/transfers_govt_pce_per_capita_1970)
@@ -53,54 +58,77 @@ scatter_dynamic = transfers %>%
 old_age_2022 = ggplot(scatter_dynamic, 
                       aes(x=share_65_over_2022, y=transfers_govt_pce_per_capita_2022)) + 
   geom_point(size=0.25)+
-  labs(title = "2022 Old Age Share vs. 2022 Per Capita Transfers", x = "population share over 65", y="per capita transfers") +
+  labs(title = "65+ share vs. per capita transfers, 2022", 
+       x = "% population 65+", 
+       y="Per capita transfers") +
   theme_minimal() +
-  theme(plot.title = element_text(face = "bold", size = 7, color = "#1a654d",hjust = 1),
-        axis.title.x = element_text(size = 7),
-        axis.title.y = element_text(size = 7),
-        axis.text.x = element_text(size=6),
-        axis.text.y = element_text(size=6))
+  scale_y_continuous(labels=comma) +
+  scale_x_continuous(labels = percent) +
+  theme(plot.title = element_text(face = "bold", size = 12, color = "black",hjust = -0.75),
+        axis.title.x = element_text(size = 12, color = "darkgrey"),
+        axis.title.y = element_text(size = 12, color = "darkgrey"),
+        axis.text.x = element_text(size=12, color = "darkgrey"),
+        axis.text.y = element_text(size=12, color = "darkgrey"))
+
 
 # static - poverty
 poverty_age_2022 = ggplot(scatter_dynamic, aes(x=Percent.below.poverty.level_2022, y=transfers_govt_pce_per_capita_2022)) + 
   geom_point(size=0.25)+
-  labs(title = "2022 Poverty Rate vs. 2022 Per Capita Transfers", x = "share under poverty threshold", y="per capita transfers") +
+  labs(title = "Poverty rate vs. per capita transfers, 2022", 
+       x = "% under poverty line", 
+       y="Per capita transfers") +
   theme_minimal() +
-  theme(plot.title = element_text(face = "bold", size = 7, color = "#1a654d",hjust = 1),
-        axis.title.x = element_text(size = 7),
-        axis.title.y = element_text(size = 7),
-        axis.text.x = element_text(size=6),
-        axis.text.y = element_text(size=6))
+  scale_y_continuous(labels=comma) +
+  scale_x_continuous(labels = percent) +
+  theme(plot.title = element_text(face = "bold", size = 12, color = "black",hjust = -1),
+        axis.title.x = element_text(size = 12, color = "darkgrey"),
+        axis.title.y = element_text(size = 12, color = "darkgrey"),
+        axis.text.x = element_text(size=12, color = "darkgrey"),
+        axis.text.y = element_text(size=12, color = "darkgrey"))
 
 # dynamic - old
 old_growth = ggplot(scatter_dynamic, aes(x=old, y=transfer)) + 
   geom_point(size=0.25)+
-  labs(title = "Old Age growth versus Transfers Growth, 1970-2022", x = "change in population share over 65", y = "change in per capita transfers") +
+  labs(title = "65+ growth vs. transfers growth, 1970-2022\n", 
+       x = "Change in population % 65+", 
+       y = "Change in per capita transfers") +
   theme_minimal() +
-  theme(plot.title = element_text(face = "bold", size = 7, color = "#1a654d",hjust = 1),
-        axis.title.x = element_text(size = 7),
-        axis.title.y = element_text(size = 7),
-        axis.text.x = element_text(size=6),
-        axis.text.y = element_text(size=6))
+  scale_x_continuous(labels = percent) +
+  scale_y_continuous(labels = percent) +
+  theme(plot.title = element_text(face = "bold", size = 12, color = "black",hjust = -1),
+        axis.title.x = element_text(size = 12, color = "darkgrey"),
+        axis.title.y = element_text(size = 12, color = "darkgrey"),
+        axis.text.x = element_text(size=12, color = "darkgrey"),
+        axis.text.y = element_text(size=12, color = "darkgrey"))
 
 # dynamic - poverty
 poverty_growth = ggplot(scatter_dynamic, aes(x=pov, y=transfer)) + 
   geom_point(size=0.25)+
-  labs(title = "Poverty growth versus Transfers Growth, 1970-2022", x = "change in poverty rate", y = "change in per capita transfers") +
+  labs(title = "Poverty growth vs. transfers growth, 1970-2022\n", 
+       x = "Change in poverty rate", 
+       y = "Change in per capita transfers") +
   theme_minimal() +
-  theme(plot.title = element_text(face = "bold", size = 7, color = "#1a654d",hjust = 1),
-        axis.title.x = element_text(size = 7),
-        axis.title.y = element_text(size = 7),
-        axis.text.x = element_text(size=6),
-        axis.text.y = element_text(size=6))
+  scale_x_continuous(labels = percent) +
+  scale_y_continuous(labels = percent) +
+  theme(plot.title = element_text(face = "bold", size = 12, color = "black",hjust =-3),
+        axis.title.x = element_text(size = 12, color = "darkgrey"),
+        axis.title.y = element_text(size = 12, color = "darkgrey"),
+        axis.text.x = element_text(size=12, color = "darkgrey"),
+        axis.text.y = element_text(size=12, color = "darkgrey"))
 
 require(gridExtra)
 
-scatter = grid.arrange(old_age_2022, poverty_age_2022, old_growth, poverty_growth)
-
+scatter = grid.arrange(old_age_2022, poverty_age_2022, old_growth, poverty_growth,
+                       top = grid::textGrob("Figure 10: Relationship between transfers, poverty, and aging\n",
+                                            hjust = 0, x = 0,
+                                            gp=grid::gpar(fontsize = 16, col = "#1a654d", fontface = "bold")),
+                       bottom = grid::textGrob("\nSource: EIG analysis of Bureau of Economic Analysis data",
+                                               hjust = 0, x = 0,
+                                               gp = grid::gpar(fontsize = 10, col="black")))
 ggsave(plot = scatter, 
-       filename = paste(path_out,"fig 10.png"),bg="white")
-
+       filename = paste(path_out,"fig 10.png",sep="/"),
+       bg="white",
+       width = 10, height = 10)
 
 # correlation of over 6 and poverty rate
 transfers_2022 = transfers %>%
