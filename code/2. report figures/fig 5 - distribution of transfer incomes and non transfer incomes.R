15f72bae913cd4ac248e83216571dba40581770f
Sarah Eckhardt <167136820+SarahMEckhardt@users.noreply.github.com>
2024-09-17 15:17:39 -0400

    Update fig 5 - distribution of transfer incomes and non transfer incomes.R

--- code/2. report figures/fig 5 - distribution of transfer incomes and non transfer incomes.R
+++ code/2. report figures/fig 5 - distribution of transfer incomes and non transfer incomes.R
@@ -1,4 +1,4 @@
-# 4 distribution of transfer incomes and non transfer incomes 
+# distribution of transfer incomes and non transfer incomes 
 # all counties, 2022
 
 # remove dependencies
@@ -23,9 +23,9 @@ plot = read_excel(paste(path_data_out, "transfers_dataset_counties_master.xlsx",
   mutate(income_no_transfers = personal_income_pce_per_capita - transfers_govt_pce_per_capita) %>%
   mutate(transfer_income = personal_income_pce_per_capita-income_no_transfers) %>%
   select(income_no_transfers, transfer_income) %>%
-  rename(`non-transfer income`= income_no_transfers,
-         `transfer income` = transfer_income) %>%
-  pivot_longer(cols = c(`non-transfer income`, `transfer income`),
+  rename(`Non-transfer income`= income_no_transfers,
+         `Transfer income` = transfer_income) %>%
+  pivot_longer(cols = c(`Non-transfer income`, `Transfer income`),
                names_to = "type") %>%
   mutate(value = value/1000) %>%
   filter(value<100)
@@ -77,18 +77,28 @@ plot %>%
   scale_fill_manual(values=c("#e1ad28", "#234f8b")) +
   theme_minimal() +
   labs(fill="", 
-       title = "Figure 5: Distribution of Transfer Incomes and Non-Transfer Incomes \n(all counties, 2022)", x = "dollars (thousands per capita)",y= "number of counties",
-       caption = "note: 15 counties with non-tranfer income over 100k per capita removed for visual clarity.") +
+       title = "Figure 5: Distribution of transfer and non-transfer incomes, 2022", 
+       x = "Dollars (thousands per capita)",
+       y= "Number of counties",
+       caption = c("Note: 15 counties with non-tranfer income >100k per capita removed for visual clarity.",
+       "\nSource: EIG analysis of Bureau of Economic Analysis data")) +
   coord_trans(y = squash_axis(300, 800, 10)) +
   scale_y_discrete(limits = c(0,100,200,300,800,900))+
-  theme(plot.title = element_text(color = "#1a654d", face="bold", size = 18),
+  theme(plot.title = element_text(color = "#1a654d", face="bold", size = 16,
+                                  hjust = -0.2),
     legend.position = "top",
-    legend.text = element_text(size = 10),
-    axis.text.x = element_text(size = 10),
-        axis.text.y = element_text(colour = c('black','black','black','red', 'red'), size = 10))
+    legend.text = element_text(size = 12, color = "darkgrey"),
+    axis.text.x = element_text(size = 12, color = "darkgrey"),
+    axis.title.x = element_text(size=12, color="darkgrey"),
+    axis.title.y = element_text(size=12, color="darkgrey"),
+    plot.caption = element_text(size = c(12,12), color = c("black","black"), hjust = c(-0.2,-0.12)),
+    axis.text.y = element_text(colour = c('darkgrey','darkgrey','darkgrey','red', 'red'), size = 12))
 
-ggsave(paste(path_out, "fig 5.png",sep="/"), bg="white")
+ggsave(paste(path_out, "fig 5.png",sep="/"), bg="white",
+       width = 10, height = 6,
+       dpi = 1000)
 
+# alternative plot --
 
 density_plot = plot %>% 
   ggplot(aes(x =value, color=type, fill=type)) +
@@ -96,7 +106,7 @@ density_plot = plot %>%
   scale_fill_manual(values=c("#e1ad28", "#234f8b")) +
   theme_minimal() +
   labs(fill="", 
-       title = "Distribution of Transfer Incomes and Non-Transfer Incomes (all counties, 2022)", 
+       title = "Distribution of Transfer Incomes and Non-Transfer\nIncomes, 2022", 
        x = "dollars (thousands per capita)",
        y= "density",
        caption = "note: 15 counties with non-tranfer income over 100k per capita removed for visual clarity.") +
