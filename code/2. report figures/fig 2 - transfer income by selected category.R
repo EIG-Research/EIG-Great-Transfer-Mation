be71258558da353ecdcd33fb84e66c4381c15f77
Sarah Eckhardt <167136820+SarahMEckhardt@users.noreply.github.com>
2024-09-27 09:34:34 -0400

    Update fig 2 - transfer income by selected category.R

--- code/2. report figures/fig 2 - transfer income by selected category.R
+++ code/2. report figures/fig 2 - transfer income by selected category.R
@@ -20,4 +20,5 @@ transfers = read_excel(paste(path_data_out, "transfers_dataset_nation_master.xls
   mutate(all_other = transfers_govt_pce - transfers_social_security_pce-transfers_medicare_pce-transfers_medicaid_pce-transfers_income_maintenance_pce-transfers_unempl_insurance_pce) %>%
   select(year, all_other, transfers_govt_pce, transfers_social_security_pce, transfers_medicare_pce, transfers_medicaid_pce, transfers_unempl_insurance_pce, transfers_income_maintenance_pce)
 
+# save for datawrapper plot
 write.xlsx(transfers, paste(path_out, "fig 2", sep="/"))
