f19f4be0c66254d76a95a69a2050ad8475fea11e
Sarah Eckhardt <167136820+SarahMEckhardt@users.noreply.github.com>
2024-09-27 14:09:24 -0400

    Update fig 2 - transfer income by selected category.R

--- code/2. report figures/fig 2 - transfer income by selected category.R
+++ code/2. report figures/fig 2 - transfer income by selected category.R
@@ -12,11 +12,11 @@ library(stringr)
 
 path_project = "ENTER USER PROJECT PATH HERE"
 path_data_raw = file.path(path_project,"data/raw")
-path_data_out = file.path(path_project,"data/clean")
+path_data_clean = file.path(path_project,"data")
 path_out = file.path(path_project,"output")
 
 # load data
-transfers = read_excel(paste(path_data_out, "transfers_dataset_nation_master.xlsx", sep = "/")) %>%
+transfers = read_excel(paste(path_data_clean, "transfers_dataset_nation_master.xlsx", sep = "/")) %>%
   mutate(all_other = transfers_govt_pce - transfers_social_security_pce-transfers_medicare_pce-transfers_medicaid_pce-transfers_income_maintenance_pce-transfers_unempl_insurance_pce) %>%
   select(year, all_other, transfers_govt_pce, transfers_social_security_pce, transfers_medicare_pce, transfers_medicaid_pce, transfers_unempl_insurance_pce, transfers_income_maintenance_pce)
 
